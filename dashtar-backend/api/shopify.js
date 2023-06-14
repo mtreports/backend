e147d788b96c0d1c7c9ade9b1d4883709d087278
mtreports <mt.reports@mandasadevelopment.com>
2023-06-14 16:36:55 +0530

    proxy change

--- dashtar-backend/api/shopify.js
+++ dashtar-backend/api/shopify.js
@@ -4,7 +4,7 @@ const {restResources} = require('@shopify/shopify-api/rest/admin/2023-04');
 const { SQLiteSessionStorage } = require("@shopify/shopify-app-session-storage-sqlite");
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5055;
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 const shopify = shopifyApp({
   api: {
@@ -15,7 +15,7 @@ const shopify = shopifyApp({
     apiVersion: LATEST_API_VERSION,
     restResources,
     hostScheme: 'http',
-    hostName: `3.6.228.94:${PORT}`,
+    hostName: `3.6.228.94:5055`,
   },
   auth: {
     path: '/api/auth',
