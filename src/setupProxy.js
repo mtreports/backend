6c946b8ee6c2eec251b8d6b49e2694d0a6cf534d
mtreports <mt.reports@mandasadevelopment.com>
2023-06-14 16:07:58 +0530

    proxy change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://3.6.228.94:5000/',
+      target: 'http://3.6.228.94:5055/',
       changeOrigin: true,
     })
   );
