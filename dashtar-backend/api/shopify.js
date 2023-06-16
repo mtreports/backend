e89b37f9ad9dbf2a051195118469a17d9972d296
Shubh <mt.reports@mandasadevelopment.com>
2023-06-16 16:10:38 +0530

    tryinggg

--- dashtar-backend/api/shopify.js
+++ dashtar-backend/api/shopify.js
@@ -14,6 +14,8 @@ const shopify = shopifyApp({
     isEmbeddedApp: false,
     apiVersion: LATEST_API_VERSION,
     restResources,
+    hostScheme: 'http',
+    hostName: `3.6.228.94:5055`,
   },
   auth: {
     path: '/api/auth',
