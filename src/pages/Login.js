52a34c523abf0df6829b37904b92a783a88c5536
mtreports <mt.reports@mandasadevelopment.com>
2023-06-21 13:05:58 +0530

    replace login heading

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
             </div>
             <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
               <div className="w-full">
-                <h1 className="mb-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Login</h1>
+                <h1 className="mb-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Login Now</h1>
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <LabelArea label="Email" />
                   <InputArea
