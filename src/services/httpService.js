857bd6482925088f5ce1b420b265ef254e9bb074
mtreports <mt.reports@mandasadevelopment.com>
2023-07-03 14:55:20 +0530

    ynjkm

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const instance = axios.create({
-  baseURL: `https://mtreports.mandasadevelopment.com/api`,
+  baseURL: `https://mtreports.mandasadevelopment.com:443/api`,
   timeout: 50000,
   headers: {
     'Accept': 'application/json',
