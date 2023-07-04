2734c61e571501d0c3e229cfa3d8c9210e250dff
mtreports <mt.reports@mandasadevelopment.com>
2023-07-04 18:30:39 +0530

    wedf

--- src/hooks/useLoginSubmit.js
+++ src/hooks/useLoginSubmit.js
@@ -17,48 +17,16 @@ const useLoginSubmit = () => {
     formState: { errors },
   } = useForm();
 
-<<<<<<< HEAD
   const onSubmit = ({ name, email, verifyEmail, password, role }) => {
-=======
-
-
-  const ondefaultLogin = ({name, email, password, role, bulkop }) => {
->>>>>>> 59b321d819ea485e3fe9fc0e1c176e712e92cef3
     setLoading(true);
     const cookieTimeOut = 0.5;
 
     if (location.pathname === '/login') {
-      AdminServices.loginAdmin({email, password})
+      AdminServices.loginAdmin({ email, password })
         .then((res) => {
           if (res) {
             setLoading(false);
-           
-            dispatch({ type: 'USER_LOGIN', payload: res });
-            Cookies.set('adminInfo', JSON.stringify(res), {
-              expires: cookieTimeOut,
-            });
-            history.replace('/dashboard');
-          }
-        })
-        .catch((err) => {
-          // notifyError(err ? err.response.data.message : err.message);
-          setLoading(false);
-        });
-    }
-
-    if (location.pathname === '/signup') {
-<<<<<<< HEAD
-      AdminServices.registerAdmin().then(()=>{
-        console.log(res);
-      })
-      AdminServices.registerAdmin({ name, email, password, role })
-=======
-      AdminServices.registerAdmin({ name, email, password, role, bulkop })
->>>>>>> 59b321d819ea485e3fe9fc0e1c176e712e92cef3
-        .then((res) => {
-          if (res) {
-            setLoading(false);
-            // notifySuccess('Register Success!');
+            notifySuccess('Login Success!');
             dispatch({ type: 'USER_LOGIN', payload: res });
             Cookies.set('adminInfo', JSON.stringify(res), {
               expires: cookieTimeOut,
@@ -67,19 +35,51 @@ const useLoginSubmit = () => {
           }
         })
         .catch((err) => {
-          // notifyError(err ? err.response.data.message : err.message);
+          notifyError(err ? err.response.data.message : err.message);
           setLoading(false);
         });
     }
 
-  };
+    if (location.pathname === '/signup') {
+      AdminServices.registerAdmin().then(()=>{
+        console.log(res);
+      })
+      AdminServices.registerAdmin({ name, email, password, role })
+        .then((res) => {
+          if (res) {
+            setLoading(false);
+            notifySuccess('Register Success!');
+            dispatch({ type: 'USER_LOGIN', payload: res });
+            Cookies.set('adminInfo', JSON.stringify(res), {
+              expires: cookieTimeOut,
+            });
+            history.replace('/');
+          }
+        })
+        .catch((err) => {
+          notifyError(err ? err.response.data.message : err.message);
+          setLoading(false);
+        });
+    }
 
+    if (location.pathname === '/forgot-password') {
+      AdminServices.forgetPassword({ verifyEmail })
+        .then((res) => {
+          setLoading(false);
+          notifySuccess(res.message);
+        })
+        .catch((err) => {
+          setLoading(false);
+          notifyError(err ? err.response.data.message : err.message);
+        });
+    }
+  };
   return {
+    onSubmit,
     register,
     handleSubmit,
     errors,
     loading,
-    ondefaultLogin,
   };
 };
 
