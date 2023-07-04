30544258691332b0e5401a820bc0b848776e64d7
mtreports <mt.reports@mandasadevelopment.com>
2023-07-04 10:43:19 +0530

    dcf

--- dashtar-backend/api/index.js
+++ dashtar-backend/api/index.js
@@ -136,9 +136,7 @@ shopify.validateAuthenticatedSession(),
 async (_req, res, _next) => {
   const shop = res.locals.shopify.session.shop;
   const accesstoken = res.locals.shopify.session.accessToken;
-  console.log(_req);
-  res.send("App works properly!");
-  //  return res.redirect(301, "http://localhost:4000");
+   return res.redirect(301, "https://mtreports.mandasadevelopment.com:443");
 }
 );
 
