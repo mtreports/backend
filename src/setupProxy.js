53c2479295f1073cf606f38c1ec5dba959775a6b
mtreports <mt.reports@mandasadevelopment.com>
2023-07-07 13:55:01 +0530

    vfbg

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+ 
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://mtreports.mandasadevelopment.com:80',
+      changeOrigin: true,
+      secure: false,
+    })
+  );
+};
