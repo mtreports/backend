16611d7ab9d5e727bcaf153d5fa9cf88271025d0
mtreports <mt.reports@mandasadevelopment.com>
2023-07-07 13:09:07 +0530

    sxdcf

--- src/index.js
+++ src/index.js
@@ -1,49 +1,28 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import "rc-tree/assets/index.css";
-import "@pathofdev/react-tag-input/build/index.css";
-import "react-loading-skeleton/dist/skeleton.css";
-import { Windmill } from "@windmill/react-ui";
-import "./assets/css/custom.css";
-import "./assets/css/tailwind.css";
-import "./assets/css/tailwind.output.css";
-import App from "./App";
-import myTheme from "./assets/theme/myTheme";
-import { AdminProvider } from "./context/AdminContext";
-import { SidebarProvider } from "./context/SidebarContext";
-import ThemeSuspense from "./components/theme/ThemeSuspense";
-import { Provider } from "react-redux";
-import store from "./redux/Store";
-import "./i18n";
-
-
-// import * as serviceWorker from './serviceWorker';
-// const countData = shopify.api.rest.Product.count({
-//   session: 'shpua_da37737532a8559c828ee4239c18c761',
-// });
-// console.log(countData);
-// if (process.env.NODE_ENV !== "production") {
-//   const axe = require("react-axe");
-//   axe(React, ReactDOM, 1000);
-// }
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'assets/css/App.css';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AuthLayout from 'layouts/auth';
+import AdminLayout from 'layouts/admin';
+import RtlLayout from 'layouts/rtl';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from 'theme/theme';
+import { ThemeEditorProvider } from '@hypertheme-editor/chakra-ui';
 
 ReactDOM.render(
-  
-  <AdminProvider>
-    <SidebarProvider>
-      <Provider store={store}>
-        <Suspense fallback={<ThemeSuspense />}>
-          <Windmill usePreferences theme={myTheme}>
-            <App />
-          </Windmill>
-        </Suspense>
-      </Provider>
-    </SidebarProvider>
-  </AdminProvider>,
-
-  document.getElementById("root")
+	<ChakraProvider theme={theme}>
+		<React.StrictMode>
+			<ThemeEditorProvider>
+				<HashRouter>
+					<Switch>
+						<Route path={`/auth`} component={AuthLayout} />
+						<Route path={`/admin`} component={AdminLayout} />
+						<Route path={`/rtl`} component={RtlLayout} />
+						<Redirect from='/' to='/admin' />
+					</Switch>
+				</HashRouter>
+			</ThemeEditorProvider>
+		</React.StrictMode>
+	</ChakraProvider>,
+	document.getElementById('root')
 );
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
